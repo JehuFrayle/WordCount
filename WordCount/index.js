e5399318c9dd36acaed03c1fc9bc4f1385b094fa
cocinero <118696492+Sam3810@users.noreply.github.com>
2023-06-15 16:48:02 -0500

    Add files via upload

---
+++ WordCount/index.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    let frase = document.getElementById('textarea').value
+    let cantidad = frase.split(' ')
+    let cantidadDividida = cantidad.length
+    let h2 = document.getElementById('h2')
+    h2.textContent = ('Su frase tiene ' + cantidadDividida + ' palabras')
+})
