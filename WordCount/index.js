22c69278bf64076a61a719d4d10fef890adb9158
cocinero <118696492+Sam3810@users.noreply.github.com>
2023-06-16 14:56:36 -0500

    Add files via upload

--- WordCount/index.js
+++ WordCount/index.js
@@ -1,18 +1,4 @@
 let button = document.getElementById('button')
-let buttonL = document.getElementById('buttonL')
-let p = document.getElementById('p')
-let number = 1
-let i = false
-
-buttonL.addEventListener('click', () => {
-    if (i == false) {
-        i = true
-        p.textContent = (number)
-    }else {
-        i = false
-        p.textContent = (number - 1)
-    }
-})
 
 button.addEventListener('click', () => {
     let frase = document.getElementById('textarea').value
