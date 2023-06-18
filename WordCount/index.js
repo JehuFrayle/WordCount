b88068ebe2aafb4e08ff9b63ca48945ecac9f4d1
Jehu Frayle <jehufrayle@jehu-tuf.localdomain>
2023-06-17 20:41:23 -0700

    Mejorado el uso de localStorage

--- WordCount/index.js
+++ WordCount/index.js
@@ -22,3 +22,18 @@ button.addEventListener('click', () => {
     let h2 = document.getElementById('h2')
     h2.textContent = ('Su frase tiene ' + cantidadDividida + ' palabras')
 })
+
+// Esta función se carga cuando se carga la página
+document.addEventListener('DOMContentLoaded', () => {
+    let numberOnMemory = localStorage.getItem('Likes') // Con esto obtenemos el valor de la memoria local
+    pL.textContent = numberOnMemory
+
+    if (numberOnMemory == null) {
+        pL.textContent = 0 // Si no hay nada en la memoria local, se pone 0
+    }
+    if(numberOnMemory > 0){
+        number = Number(numberOnMemory) // Si hay algo en la memoria local, se pone ese valor
+        // Se utiliza Number() para convertir el valor de la memoria local en un número, porque por defecto es un string
+        i = true // Actualizamos el valor de i para no romper la lógica del botón
+    }
+})
